cc1e2d89a55b359aea21092bcfe72cc2606c94b4
Kiphea <djsmyth05@gmail.com>
2025-11-09 11:55:13 +0000

    Task 4 Complete

--- SixthPractical-MyArrayProject/Program.cs
+++ SixthPractical-MyArrayProject/Program.cs
@@ -1,19 +1,34 @@
-﻿class Program
+﻿using System.Diagnostics.Metrics;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        int lowerCaseAlphabetChar = 97;
+        int lowerCaseAlphabetStart = 97;
+        int lowerCaseAlphabetEnd = 122;
         string[] alphabet = new string[26];
 
-        for (int i = 0; i < alphabet.Length - 1; i++)
+        //Task 3 Beginning
+        Console.WriteLine("---------------------------");
+        Console.Write("Task 3 - ");
+
+        for (int i = 0; i < alphabet.Length; i++)
         {
-            alphabet[i] = Convert.ToString((char)lowerCaseAlphabetChar);
-            lowerCaseAlphabetChar += 1;
+            alphabet[i] = Convert.ToString((char)lowerCaseAlphabetStart);
+            lowerCaseAlphabetStart += 1;
+            Console.Write(alphabet[i]);
         }
 
-        foreach (string letter in alphabet)
+        //Task 4 Beginning
+        Console.WriteLine("");
+        Console.WriteLine("---------------------------");
+        Console.Write("Task 4 - ");
+
+        for (int i = 0; i < alphabet.Length; i++)
         {
-            Console.Write(letter);
+            alphabet[i] = Convert.ToString((char)lowerCaseAlphabetEnd);
+            lowerCaseAlphabetEnd -= 1;
+            Console.Write(alphabet[i]);
         }
     }
 }
