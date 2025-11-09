167d860fd189ca8a58548e0032e53a00177258a9
Kiphea <djsmyth05@gmail.com>
2025-11-09 11:47:25 +0000

    Task 3 Complete

--- SixthPractical-MyArrayProject/Program.cs
+++ SixthPractical-MyArrayProject/Program.cs
@@ -2,6 +2,18 @@
 {
     public static void Main(string[] args)
     {
+        int lowerCaseAlphabetChar = 97;
         string[] alphabet = new string[26];
+
+        for (int i = 0; i < alphabet.Length - 1; i++)
+        {
+            alphabet[i] = Convert.ToString((char)lowerCaseAlphabetChar);
+            lowerCaseAlphabetChar += 1;
+        }
+
+        foreach (string letter in alphabet)
+        {
+            Console.Write(letter);
+        }
     }
 }
